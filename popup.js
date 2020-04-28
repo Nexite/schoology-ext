e7386f2215b03bfbcef458d8aaa99f2f39d3ff44
jsadowyj0 <sadowyjj@gmail.com>
2020-04-28 05:37:23 -0400

    refactored popup.js

--- popup.js
+++ popup.js
@@ -11,52 +11,54 @@ let data;
 
 const addInfo = function(str, append) {
   if (append) return info.innerHTML += str + '<br>';
-  info.innerHTML = str;
+  info.innerHTML = str + '<br>';
 }
 
 const resetStorage = function() {
   chromeStorage.set({ "schoology-data": [] }, function() {
     chromeStorage.get('schoology-data', function(result){
       console.log(result)
-      // addInfo(`New Data: ${JSON.stringify(result)}`, false);
+    })
+  });
+}
+
+const getStorage = function() {
+  chromeStorage.get('schoology-data', function(rawData) {
+    
+    data = rawData['schoology-data'];
+  
+    if (typeof data === 'undefined') {
+      console.log('Schoology data is undefined! Resetting...')
+      resetStorage();
+      getStorage();
+    } else {
+      uploadStorage();
+    }
+  })
+}
+
+const uploadStorage = function() {
+  let duplicateValue = false;
+  
+  data.forEach(obj => { if(Number(obj.id) === Number(input.value)) duplicateValue = true } )
+
+  if(duplicateValue) return addInfo('No Duplicate Values!', false);
+
+  data.push({ id: Number(input.value) });
+
+  chromeStorage.set({ "schoology-data": data }, function() {;
+    chromeStorage.get('schoology-data', function(result){
+      console.log(result);
+      addInfo(`ID Uploaded (${input.value})`, true)
+      input.value = '';
     })
   });
 }
 
 uploadBtn.addEventListener('click', () => {
-
   clearText.innerHTML = '';
-
   if (input.value === '') {chrome.storage.local.get(function(result){console.log(result)}); return}
-
-  chromeStorage.get('schoology-data', function(rawData) {
-    
-    data = rawData['schoology-data'];
-
-    if (typeof data === 'undefined') {
-      console.log('Schoology data is undefined! Resetting...')
-      resetStorage();
-      addInfo('Schoology data is undefined... Try entering the ID again.', false);
-    } else {
-
-      let duplicateValue = false;
-
-      data.forEach(obj => { if(Number(obj.id) === Number(input.value)) duplicateValue = true } )
-
-      if(duplicateValue) return info.innerHTML = 'No Duplicate Values!</br>';
-
-      data.push({ id: Number(input.value) });
-
-      chromeStorage.set({ "schoology-data": data }, function() {;
-        chromeStorage.get('schoology-data', function(result){
-          console.log(result);
-          info.innerHTML += `ID Uploaded (${input.value})<br>`
-          input.value = '';
-        })
-      });
-    }
-  })
-
+  getStorage();
 })
 
 // removeBtn.addEventListener('click', function() {
@@ -67,11 +69,10 @@ uploadBtn.addEventListener('click', () => {
 
 
 clearBtn.addEventListener('click', function() {
-
   resetStorage();
+  addInfo('', false);
   clearText.innerHTML = clearCount === 0 ? 'Cleared!' : `Cleared ${clearCount + 1} times!`
   clearCount++
-
 })
 
 input.addEventListener('click', () => {
