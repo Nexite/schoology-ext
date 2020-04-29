e301b40078e69f0a25b49f067b5d42c9c1ef1ced
jsadowyj0 <sadowyjj@gmail.com>
2020-04-29 02:53:59 -0400

    (hopefully) fixed the submission bug

--- async-assignment.js
+++ async-assignment.js
@@ -2,19 +2,21 @@ const urlPath = window.location.pathname;
 const assignmentId = Number(urlPath.split('/')[2]);
 const chromeStorage = chrome.storage.local;
 
-const submitBtn = document.querySelector(`a[href="/assignment/${assignmentId}/dropbox/submit"]`);
-
+let submitBtn = document.querySelector(`a[href="/assignment/${assignmentId}/dropbox/submit"]`);
+// let assignments;
 let schoologyData;
 
 // jQuery Document Ready
 $(function() {
 
-  assignments = document.querySelectorAll("div.upcoming-event.course-event a");
+  // assignments = document.querySelectorAll("div.upcoming-event.course-event a");
+  submitBtn = document.querySelector(`a[href="/assignment/${assignmentId}/dropbox/submit"]`);
+  if (submitBtn === null) return console.log('One or more variables is undefined... Report to developer.');
 
   getStorage();
 
   const btnInterval = setInterval(function() {
-    if (submitBtn.textContent !== 'Submit Assignment') {
+    if (submitBtn.textContent !== 'Submit Assignment' || $(`a[href="/assignment/${assignmentId}/dropbox/submit"]`).text() !== 'Submit Assignment' ) {
 
       clearInterval(btnInterval);
 
@@ -23,7 +25,9 @@ $(function() {
       return console.log('Assignment submitted! Clearing interval and executing getStorage()');
 
     }
-    
+
+    submitBtn = document.querySelector(`a[href="/assignment/${assignmentId}/dropbox/submit"]`);
+
     console.log('Assignment not submitted...');
 
   }, 500)
